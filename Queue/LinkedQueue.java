2198ed3681779fce46a4c40f3a971043578e3f3d
Ghous bux <ghousbux430@gmail.com>
2026-08-23 12:48:00 -0800

    some changes

--- Queue/LinkedQueue.java
+++ Queue/LinkedQueue.java
@@ -57,27 +57,74 @@ public class LinkedQueue implements Queue {
         return temp;
     }
 
+
+    public void isRemaining(LinkedQueue credit, LinkedQueue debit){
+        int remaining = credit.sum() - debit.sum();
+
+        if(remaining>0){
+            System.out.println("Remaining balance in my pocket is : "+remaining);
+        }else if(remaining<0){
+            System.out.println("I spent "+remaining+" extra");
+        }else{
+            System.out.println("Neither remaining nor i extra spent");
+        }
+    }
+
+    public int sum(){
+        int sum = 0;
+        Node p = head.next;
+        while(p!=head){
+            sum+=(int)p.data;
+
+            p = p.next;
+        }
+        return sum;
+    }
+
+    public String toString() {
+        if (size == 0) {
+            throw new IllegalStateException("queue is empty");
+        }
+        StringBuffer sb = new StringBuffer();
+        Node p = head.next;
+        while (p != head) {
+            sb.append(p.data);
+            if(p.next != head){
+                sb.append(", ");
+            }
+            p = p.next;
+        }
+        return sb.toString();
+
+    }
+
     public static void main(String[] args) {
 
-        LinkedQueue q = new LinkedQueue();
+         LinkedQueue credit = new LinkedQueue();
+        credit.add(200);
+        credit.add(100);
+        credit.add(250);
+        credit.add(500);
+        credit.add(150);
+        credit.add(80);
+        credit.add(400);
 
-        q.add(10);
-        q.add(20);
-        q.add(30);
-        q.add(40);
+        LinkedQueue debit = new LinkedQueue();
 
-        System.out.println("Queue size: " + q.size());
-        System.out.println("Queue First element: " + q.first());
+        debit.add(90);
+        debit.add(190);
+        debit.add(120);
+        debit.add(340);
+        debit.add(60);
+        debit.add(50);
+        debit.add(250);
 
-        System.out.println("Removed: " + q.remove());
-        System.out.println("Removed: " + q.remove());
+        System.out.println("each day credit in a week : "+credit);
+        System.out.println("each day debit in a week : "+debit);
 
-        System.out.println("Queue size after removal: " + q.size());
-        System.out.println("First element now: " + q.first());
+        System.out.println("sum of credit is : "+credit.sum());
+        System.out.println("sum of debit is : "+debit.sum());
+        credit.isRemaining(credit,debit);
 
-        System.out.println("Removed: " + q.remove());
-        System.out.println("Removed: " + q.remove());
-
-        System.out.println("Queue size: " + q.size());
     }
 }
