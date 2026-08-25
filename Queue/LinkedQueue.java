e6fa44fadf0d1f7303a93761e877aaaf4918fb50
Ghous bux <ghousbux430@gmail.com>
2026-08-24 16:31:38 -0800

    changes

--- Queue/LinkedQueue.java
+++ Queue/LinkedQueue.java
@@ -77,6 +77,7 @@ public class LinkedQueue implements Queue {
         Node p = head.next;
         while(p!=head){
             sum+=(int)p.data;
+
             p = p.next;
         }
         return sum;
@@ -96,6 +97,7 @@ public class LinkedQueue implements Queue {
             p = p.next;
         }
         return sb.toString();
+
     }
 
     public static void main(String[] args) {
